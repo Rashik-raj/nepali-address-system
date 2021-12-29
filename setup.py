d125dc45b85b68704880fa86d65bd4a33c9becbd
rashik-raj <rashik123.rs@gmail.com>
2021-12-29 15:52:04 +0545

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nepali-address-system",
-    version="0.0.2",
+    version="0.0.3",
     author="Rashikraj Shrestha",
     author_email="rashik123.rs@gmail.com",
     description="Package to work with nepali address system",
